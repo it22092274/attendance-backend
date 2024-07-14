12346bfaf21386fbc7fae0d1fb347f7d0605978c
Vihanga Mallawaarachchi <128246276+it22092274@users.noreply.github.com>
2024-07-14 11:52:21 +0530

    Update Lecturer_Auth_Router.js

--- routes/Lecturer_Auth_Router.js
+++ routes/Lecturer_Auth_Router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { lecturer_login, profile, resetPassword } = require('../controllers/Lecturer_Auth_Controller');
 
-router.post('/login', cors(corsOptions), lecturer_login);
+router.post('/login', lecturer_login);
 router.post('/profile', profile);
 router.post('/reset-password', resetPassword);
 
